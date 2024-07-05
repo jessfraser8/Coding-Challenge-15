129c62991da9651a58057919662c68c3fce76a00
jessfraser8 <jfraser4@usf.edu>
2024-07-04 21:24:18 -0400

    Create experience section with reusable components.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,15 +9,18 @@ import React from 'react';
 // Link to Dashboard for skills section.
 import Dashboard from './Dashboard.js'
 
+// Link to Experience for experience section.
+import Experience from './Experience.js'
+
 // Personal Information section. 
 const user ={
   name: "Jessica Fraser",
-  bio: "I am a senior at the University of South Florida, after transfering from Hillsborough Community College with an AA. I am an applied math major with a concentration in data analytics and business intelligence. I am also getting a BAIS certificate as my minor.",
+  bio: "I am a senior at the University of South Florida, after transfering from Hillsborough Community College with an AA. I am an applied math major with a concentration in data analytics and business intelligence. I am also getting a Business Analytics and Information Systems certificate as my minor.",
   contact: "jfraser4@usf.edu"
 }
 
 // Create webpage.
-export default function Profile() {
+export default function App() {
   return(
     <>
     {/* Personal information section. */}
@@ -31,6 +34,7 @@ export default function Profile() {
     <div className="App">
       <Dashboard skills={skills}/>
     </div>
+    <Experience/>
     </>
   )
 }
---
+++ my-app/src/Experience.js
@@ -0,0 +1,13 @@
+// Create reusable component for past work experience.
+
+export default function Experience() {
+    return (
+        <>
+        <h3>Experience: </h3>
+        <p><u>Intern - Exeter</u></p>
+        <p>May-September 2019</p>
+        <p><u>Math Tutor - Hillsborough Community College</u></p>
+        <p>September 2023-present</p>
+        </>
+    )
+}
